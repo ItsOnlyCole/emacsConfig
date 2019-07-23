90531e6d4aefc9132067f03518733ff657eea0c7
Cole Hemp <ColeHemp@gmail.com>
2019-07-22 21:20:42 -0400

    Add Melpa & Nyan Mode

---
+++ init.el
@@ -0,0 +1,30 @@
+;;------------------------------;;
+;;          ItsOnlyCole         ;;
+;;          EmacsConfig         ;;
+;;------------------------------;;
+
+
+(package-initialize)
+
+;;Sets Up Melpa Package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (nyan-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+ ;;---Nyan Mode Config---;;
+ (nyan-mode 1)
