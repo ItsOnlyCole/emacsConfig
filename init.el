a15fd7b8eea57e86927df592762304eed7ca2d6e
Cole Hemp <ColeHemp@gmail.com>
2019-07-22 21:52:55 -0400

    Enable LineNums

--- init.el
+++ init.el
@@ -12,16 +12,11 @@
 
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(display-line-numbers t)
  '(package-selected-packages (quote (nyan-mode))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
 
 
