0b9903d690d7f45705a39d979da15d7d5372b241
Cole Hemp <ColeHemp@gmail.com>
2019-07-23 21:52:36 -0400

    Add Multiple Cursors & Smart-Mode-Line

--- init.el
+++ init.el
@@ -17,10 +17,13 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-safe-themes
+   (quote
+    ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
  '(display-line-numbers t)
  '(package-selected-packages
    (quote
-    (dashboard all-the-icons page-break-lines shell-pop restart-emacs centaur-tabs nyan-mode)))
+    (multiple-cursors smart-mode-line-atom-one-dark-theme smart-mode-line dashboard all-the-icons page-break-lines shell-pop restart-emacs centaur-tabs nyan-mode)))
  '(shell-pop-universal-key "C-t"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -71,11 +74,20 @@
 (require 'ido)
 (ido-mode t)
 
+;;---Multiple Cursors---;;
+(require 'multiple-cursors)
+;;Keybinds are in Custom Keybinds;;
+
 ;;---Nyan Mode Config---;;
 (if window-system (nyan-mode 1))
 
 ;;---Page Break Lines---;;
 (require 'page-break-lines)
+
+;;---Smart Mode Line---;
+(require 'smart-mode-line)
+(setq sml/theme 'atom-one-dark)
+(sml/setup)
 
 ;;----------------------;;
 ;;        Custom        ;;
@@ -88,3 +100,10 @@
 ;;---Centaur Tabs---;;
 (global-set-key (kbd "C-<left>") 'centaur-tabs-backward)
 (global-set-key (kbd "C-<right>") 'centaur-tabs-forward)
+
+;;---Multiple Cursors---;;
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
